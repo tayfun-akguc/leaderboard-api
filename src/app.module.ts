8a9ecaa54a56801d2152e7211af1fc6b66cb6e6a
Tayfun <dev.akguc@gmail.com>
2024-11-16 17:24:42 +0300

    set application logger as pino logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PinoLoggerModule } from './shared';
 
 @Module({
-  imports: [],
+  imports: [PinoLoggerModule],
   controllers: [AppController],
   providers: [AppService],
 })
