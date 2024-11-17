dd8d082d26c0187c1cb2b5ec98216d84be319350
Tayfun <dev.akguc@gmail.com>
2024-11-17 17:59:55 +0300

    unit test coverage

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -3,6 +3,7 @@ import { UserService } from './user.service';
 import { getModelToken } from '@nestjs/mongoose';
 import { User } from './schema/user.schema';
 import { Model } from 'mongoose';
+import { HttpException, NotFoundException } from '@nestjs/common';
 
 const mockUserDoc = {
   _id: '1234',
@@ -25,6 +26,7 @@ describe('UserService', () => {
           useValue: {
             new: jest.fn().mockResolvedValue(mockUserDoc),
             create: jest.fn(),
+            findOne: jest.fn(),
           },
         },
       ],
@@ -43,15 +45,78 @@ describe('UserService', () => {
       .spyOn(userModel, 'create')
       .mockImplementationOnce(() => Promise.resolve(mockUserDoc as any));
 
-    const newUser = await userService.createUser({
+    jest.spyOn(userModel, 'findOne').mockImplementationOnce(() => null);
+
+    const result = await userService.createUser({
       username: 'username',
       password: 'password',
     });
 
-    expect(newUser.id).toEqual(mockUserDoc._id);
-    expect(newUser.username).toEqual(mockUserDoc.username);
-    expect(newUser.createdAt).toEqual(mockUserDoc.createdAt);
-    expect(newUser.updatedAt).toEqual(mockUserDoc.updatedAt);
+    expect(result.id).toEqual(mockUserDoc._id);
+    expect(result.username).toEqual(mockUserDoc.username);
+    expect(result.createdAt).toEqual(mockUserDoc.createdAt);
+    expect(result.updatedAt).toEqual(mockUserDoc.updatedAt);
     expect(userModel.create).toHaveBeenCalledTimes(1);
+  });
+
+  it('should throw an error when the username is already in use', async () => {
+    jest.spyOn(userModel, 'findOne').mockResolvedValue(mockUserDoc);
+
+    try {
+      await userService.createUser({
+        username: 'username',
+        password: 'password',
+      });
+    } catch (error) {
+      expect(error).toBeInstanceOf(HttpException);
+      expect(error.status).toEqual(409);
+      expect(userModel.findOne).toHaveBeenCalledTimes(1);
+      expect(userModel.create).toHaveBeenCalledTimes(0);
+    }
+  });
+
+  it('should return user details when found', async () => {
+    jest.spyOn(userModel, 'findOne').mockResolvedValue(mockUserDoc);
+
+    const result = await userService.findUser('username');
+
+    expect(userModel.findOne).toHaveBeenCalledTimes(1);
+    expect(result.id).toEqual(mockUserDoc._id);
+    expect(result.username).toEqual(mockUserDoc.username);
+  });
+
+  it('should throw exception when user not found', async () => {
+    jest.spyOn(userModel, 'findOne').mockResolvedValue(null);
+
+    try {
+      await userService.findUser('username');
+    } catch (error) {
+      expect(error).toBeInstanceOf(NotFoundException);
+      expect(userModel.findOne).toHaveBeenCalledTimes(1);
+    }
+  });
+
+  it('should return true for correct user credentials', async () => {
+    jest.spyOn(userModel, 'findOne').mockResolvedValue(mockUserDoc);
+
+    const result = await userService.validateCredentials(
+      'username',
+      'password',
+    );
+
+    expect(result).toEqual(true);
+    expect(userModel.findOne).toHaveBeenCalledTimes(1);
+  });
+
+  it('should return false for wrong user credentials', async () => {
+    jest.spyOn(userModel, 'findOne').mockResolvedValue(null);
+
+    const result = await userService.validateCredentials(
+      'username',
+      'password',
+    );
+
+    expect(result).toEqual(false);
+    expect(userModel.findOne).toHaveBeenCalledTimes(1);
   });
 });
