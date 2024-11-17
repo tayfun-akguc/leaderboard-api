ed2a26d4c4b8ea2892ea489daf84e7c7037ad1ae
Tayfun <dev.akguc@gmail.com>
2024-11-17 17:59:40 +0300

    validate and findUser feats

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, Logger } from '@nestjs/common';
+import {
+  HttpException,
+  Injectable,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schema/user.schema';
 import { Model } from 'mongoose';
@@ -13,14 +18,55 @@ export class UserService {
   ) {}
 
   async createUser(createUserDto: CreateUserDto): Promise<UserDto> {
-    const user = await this.userModel.create({ ...createUserDto });
+    const user = await this.userModel.findOne({
+      username: createUserDto.username,
+    });
+    if (user) {
+      throw new HttpException(
+        {
+          message: 'Username already in use!',
+          statusCode: 409,
+        },
+        409,
+      );
+    }
+    const newUser = await this.userModel.create({ ...createUserDto });
     const response = new UserDto(
+      newUser._id.toString(),
+      newUser.username,
+      newUser.createdAt,
+      newUser.updatedAt,
+    );
+    this.logger.log({ ...response }, 'New user created!');
+    return response;
+  }
+
+  async validateCredentials(
+    username: string,
+    password: string,
+  ): Promise<boolean> {
+    const user = await this.userModel.findOne({
+      username: username,
+      password: password,
+    });
+    if (!user) {
+      return false;
+    }
+    return true;
+  }
+
+  async findUser(username: string) {
+    const user = await this.userModel.findOne({
+      username: username,
+    });
+    if (!user) {
+      throw new NotFoundException('User not found!');
+    }
+    return new UserDto(
       user._id.toString(),
       user.username,
       user.createdAt,
       user.updatedAt,
     );
-    this.logger.log({ ...response }, 'New user created!');
-    return response;
   }
 }
